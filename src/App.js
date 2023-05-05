b5b48c1b9800ad737230821de9ae1ba92fe52f0d
Shubham Bhatt <sbhatt@transcloud.co.in>
2023-05-05 20:55:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+    const [count,setCount] = useState(10);
+
+    const someStyle = {
+        background:"lightblue",
+        color:"white",
+        fontSize:"35px",
+        fontWeight:"bold",
+    };
+
+    function decrement()
+    {
+        // setCount(count-1);
+        setCount(prevCount => prevCount - 1);
+    }
+
+    function increment()
+    {
+        // setCount(count+1);
+        setCount(prevCount => prevCount + 1);
+    }
   return (
     <div className="App">
       <header className="App-header">
+          <div>
+              <span>{count}</span>
+              <button onClick={decrement}>-</button>
+              <button onClick={increment}>+</button>
+          </div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={someStyle}>
+          Edit <code>src/App.js</code> and save to reload - nice page refresh without reload ! NICE REACT
+          <br/>
+          {50 / 5}
+<br/>
+          {10 * 2 }
         </p>
         <a
           className="App-link"
